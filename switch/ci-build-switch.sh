ebb317df7791d73665a0319db0bac4d98b85254c
Sergio Padrino <sergio.padrino@gmail.com>
2020-05-05 19:10:15 +0200

    Add GH action to create release

--- switch/ci-build-switch.sh
+++ switch/ci-build-switch.sh
@@ -3,3 +3,11 @@
 source /etc/profile.d/devkit-env.sh
 cd switch
 make -j8
+
+# Bundle and prepare for release
+mkdir -p switch/OpenSupaplex
+mv OpenSupaplex.nro switch/OpenSupaplex/OpenSupaplex.nro
+wget https://cdn.chills.co.za/supaplex.zip
+unzip supaplex.zip -d switch/OpenSupaplex/
+rm switch/OpenSupaplex/*.exe
+zip -r OpenSupaplex-switch.zip switch
