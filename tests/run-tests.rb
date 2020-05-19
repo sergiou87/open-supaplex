a50296e5773d38772defbb7714c4ed577fd89275
Sergio Padrino <sergio.padrino@gmail.com>
2020-05-19 16:46:21 +0200

    Run less tests simultaneously

--- tests/run-tests.rb
+++ tests/run-tests.rb
@@ -18,7 +18,7 @@ end
 $gamePath = File.dirname($gameBinaryPath)
 $gameBinary = File.basename($gameBinaryPath)
 
-$maxSimultaneousTests = Etc.nprocessors * 8
+$maxSimultaneousTests = Etc.nprocessors * 4
 
 puts "Running #{$maxSimultaneousTests} tests simultaneously"
 
