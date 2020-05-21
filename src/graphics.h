4f0968485e7756998ad75acec20d3f63f1f30ca4
Sergio Padrino <sergio.padrino@gmail.com>
2020-05-22 00:47:09 +0200

    Extract some method to the graphics module

--- src/graphics.h
+++ src/graphics.h
@@ -79,6 +79,15 @@ extern uint8_t gScrollDestinationScreenBitmapData[kFullScreenFramebufferLength];
 extern uint8_t gLevelBitmapData[kLevelBitmapWidth * kLevelBitmapHeight];
 
 void readMenuDat(void);
+void loadMurphySprites(void);
+void readPanelDat(void);
+void readBackDat(void);
+void readBitmapFonts(void);
+void readAndRenderTitleDat(void);
+void readAndRenderTitle1Dat(void);
+void readTitle2Dat(void);
+void readGfxDat(void);
+void readControlsDat(void);
 
 void drawLevelViewport(uint16_t x, uint16_t y, uint16_t width, uint16_t height);
 void drawCurrentLevelViewport(uint16_t panelHeight);
