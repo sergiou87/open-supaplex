dd12d21d032d68dd2616462f680ef1bf363d8ebb
Sergio Padrino <sergio.padrino@gmail.com>
2020-06-01 21:25:59 +0200

    Build 3DS nightlies from CI

---
+++ 3ds/ci-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /etc/profile.d/devkit-env.sh
+cd 3ds
+make -j8 || exit 1
+
+# Bundle and prepare for release
+mkdir OpenSupaplex
+mkdir 3ds
+mkdir cias
+mv OpenSupaplex.3dsx 3ds/OpenSupaplex.3dsx
+mv OpenSupaplex.cia cias/OpenSupaplex.cia
+cp -R ../resources/* OpenSupaplex/
+rm -rf OpenSupaplex/audio-hq # we don't need high quality audio here
+zip -r OpenSupaplex-3ds.zip OpenSupaplex 3ds cias INSTALL.TXT
