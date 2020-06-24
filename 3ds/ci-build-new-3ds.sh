baed46eb0749c76a1ae00d527e9a8f86c74a0e09
Sergio Padrino <sergio.padrino@gmail.com>
2020-06-24 23:57:27 +0200

    Update build scripts to generate 2 builds of 3DS game, one with better audio quality and performance

--- 3ds/ci-build-new-3ds.sh
+++ 3ds/ci-build-new-3ds.sh
@@ -2,7 +2,7 @@
 
 source /etc/profile.d/devkit-env.sh
 cd 3ds
-make -j8 || exit 1
+make -j8 NEW3DS=1 || exit 1
 
 # Bundle and prepare for release
 mkdir OpenSupaplex
@@ -12,5 +12,5 @@ mv OpenSupaplex.3dsx 3ds/OpenSupaplex.3dsx
 mv OpenSupaplex.cia cias/OpenSupaplex.cia
 cp -R ../resources/* OpenSupaplex/
 rm -rf OpenSupaplex/audio-hq # we don't need high quality audio here
-rm -rf OpenSupaplex/audio-mq # we don't need medium quality audio here
-zip -r OpenSupaplex-3ds.zip OpenSupaplex 3ds cias INSTALL.TXT
+rm -rf OpenSupaplex/audio-lq # we don't need low quality audio here
+zip -r OpenSupaplex-new-3ds.zip OpenSupaplex 3ds cias INSTALL.TXT
---
+++ 3ds/ci-build-old-3ds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /etc/profile.d/devkit-env.sh
+cd 3ds
+make -j8 NEW3DS=0 || exit 1
+
+# Bundle and prepare for release
+mkdir OpenSupaplex
+mkdir 3ds
+mkdir cias
+mv OpenSupaplex.3dsx 3ds/OpenSupaplex.3dsx
+mv OpenSupaplex.cia cias/OpenSupaplex.cia
+cp -R ../resources/* OpenSupaplex/
+rm -rf OpenSupaplex/audio-hq # we don't need high quality audio here
+rm -rf OpenSupaplex/audio-mq # we don't need medium quality audio here
+zip -r OpenSupaplex-old-3ds.zip OpenSupaplex 3ds cias INSTALL.TXT
