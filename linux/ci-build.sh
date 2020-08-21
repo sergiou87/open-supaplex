d678a983332cfd61531a2b06037fda03dd913a19
Sergio Padrino <sergio.padrino@gmail.com>
2020-08-21 11:47:51 +0200

    Fix CI build for Linux

--- linux/ci-build.sh
+++ linux/ci-build.sh
@@ -1,5 +1,8 @@
-make -j8 -f Makefile.tests || exit
+cd linux
+make -j8 -f Makefile || exit
 cp -R ../resources ./OpenSupaplex-linux
 rm -rf ./OpenSupaplex-linux/audio-lq # we don't need low quality audio here
 rm -rf ./OpenSupaplex-linux/audio-mq # we don't need medium quality audio here
 cp opensupaplex ./OpenSupaplex-linux/OpenSupaplex
+
+zip -r OpenSupaplex-linux.zip ./OpenSupaplex-linux
