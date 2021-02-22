5b450081c2ae8eaa30f493ec1b6df9b6a30fa38f
Sergio Padrino <sergio.padrino@gmail.com>
2021-02-22 10:37:13 +0100

    Bump audio quality for PSP

--- psp/ci-build.sh
+++ psp/ci-build.sh
@@ -7,6 +7,6 @@ make -j8 || exit 1
 mkdir OpenSupaplex
 mv EBOOT.PBP OpenSupaplex/EBOOT.PBP
 cp -R ../resources/* OpenSupaplex/
-rm -rf OpenSupaplex/audio-hq # we don't need high quality audio here
+rm -rf OpenSupaplex/audio-mq # we don't need medium quality audio here
 rm -rf OpenSupaplex/audio-lq # we don't need low quality audio here
 zip -r OpenSupaplex-psp.zip OpenSupaplex
