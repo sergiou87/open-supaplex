8404d54e0d147882c27f4626d44aa9bbe555891f
MrHuu <wilbert.huu@gmail.com>
2021-09-26 18:24:38 +0200

    (3DS) Move distributed .3dsx to writable directory

--- 3ds/ci-build-3ds.sh
+++ 3ds/ci-build-3ds.sh
@@ -6,7 +6,8 @@ make -j8 || exit 1
 
 # Bundle and prepare for release
 mkdir 3ds
+mkdir 3ds/OpenSupaplex
 mkdir cia
-mv OpenSupaplex.3dsx 3ds/OpenSupaplex.3dsx
+mv OpenSupaplex.3dsx 3ds/OpenSupaplex/OpenSupaplex.3dsx
 mv OpenSupaplex.cia cia/OpenSupaplex.cia
 zip -r OpenSupaplex-3ds.zip 3ds cia INSTALL.TXT
