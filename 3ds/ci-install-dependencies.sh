6e38aa3b643a066209fe85f0ec062e57055d6107
MMM1150 <116541746+MMM1150@users.noreply.github.com>
2022-10-29 23:49:16 +0700

    Delete 3ds directory

--- 3ds/ci-install-dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update && sudo apt-get -y install zip
-sudo dkp-pacman -Sy && sudo dkp-pacman -S dkp-libs/3ds-sdl --noconfirm # not needed once the devkitpro/devkitarm image is updated
