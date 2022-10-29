e8c3b7fd53ccb09d46fd72ba6dec3d161fcd61e0
MMM1150 <116541746+MMM1150@users.noreply.github.com>
2022-10-29 13:16:00 +0700

    Delete ci-build.sh

--- macos/ci-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd macos
-#xcodebuild -project OpenSupaplex/OpenSupaplex.xcodeproj -scheme OpenSupaplex -configuration Release
-xcodebuild -project OpenSupaplex/OpenSupaplex.xcodeproj -scheme OpenSupaplex -configuration Release -archivePath ./OpenSupaplex-macOS.xcarchive archive
-pushd OpenSupaplex-macOS.xcarchive/Products/Applications
-zip -r ../../../OpenSupaplex-macOS.zip OpenSupaplex.app
-popd
