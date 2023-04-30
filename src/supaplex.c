6be1dea6ed9644e350e316c62d551bfcb7476c83
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-04-30 19:33:41 +0200

    Improve errno when checking supaplex.cfg

--- src/supaplex.c
+++ src/supaplex.c
@@ -1963,12 +1963,7 @@ void readConfig() //  proc near       ; CODE XREF: start:loc_46F0Fp
     FILE *file = openWritableFile("SUPAPLEX.CFG", "rb");
     if (file == NULL)
     {
-    // PS2 for now is not returning errno code properly
-#ifdef __PS2__
-        if (1)
-#else
-        if (errno == ENOENT || errno == ENOSYS) // ax == 2? ax has error code, 2 is file not found (http://stanislavs.org/helppc/dos_error_codes.html)
-#endif
+    if (errno == ENOENT || errno == ENOSYS || errno == EIO) // ax == 2? ax has error code, 2 is file not found (http://stanislavs.org/helppc/dos_error_codes.html)
         {
 //loc_47551:              //; CODE XREF: readConfig+Fj
                        // ; readConfig+17j
