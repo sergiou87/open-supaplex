6f3c925601b6aecd87c1f39207749bfacc110715
Sergio Padrino <sergio.padrino@gmail.com>
2026-03-21 17:52:16 +0100

    PS2 CI: produce zipped PS2 bundle
    Update PS2 CI to build and package a zipped PS2 bundle instead of a raw ELF. Changes: update .github/platform-builds.json artifact_path to OpenSupaplex-ps2.zip, add OpenSupaplex and zip entries to .gitignore, install zip in ci/Dockerfile-ps2, and extend ps2/ci-build.sh to create an OpenSupaplex folder, copy the ELF and resources into it, and produce OpenSupaplex-ps2.zip.

--- ps2/ci-build.sh
+++ ps2/ci-build.sh
@@ -3,5 +3,15 @@
 set -eu
 
 cd "$(dirname "$0")"
+
+bundle_dir=OpenSupaplex
+
 make clean
 make -j8
+
+rm -rf "$bundle_dir" OpenSupaplex-ps2.zip
+mkdir "$bundle_dir"
+cp OpenSupaplex-debug.elf "$bundle_dir"/
+cp -R ../resources/* "$bundle_dir"/
+
+zip -r OpenSupaplex-ps2.zip "$bundle_dir"
