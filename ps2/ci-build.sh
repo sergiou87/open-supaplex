1521424d90a178eab44bd77b4885da8078a41f45
Sergio Padrino <sergio.padrino@gmail.com>
2026-03-21 23:11:59 +0100

    PS2: add release/debug targets and update CI packaging
    Split PS2 build targets into debug and release (TARGET_DEBUG / TARGET_RELEASE), select the correct target based on DEBUG, and adjust clean/pack steps to produce a packed release ELF. Also update CI script to copy the release OpenSupaplex.elf into the build bundle instead of the debug binary.

--- ps2/ci-build.sh
+++ ps2/ci-build.sh
@@ -11,7 +11,7 @@ make -j8
 
 rm -rf "$bundle_dir" OpenSupaplex-ps2.zip
 mkdir "$bundle_dir"
-cp OpenSupaplex-debug.elf "$bundle_dir"/
+cp OpenSupaplex.elf "$bundle_dir"/
 cp -R ../resources/* "$bundle_dir"/
 
 zip -r OpenSupaplex-ps2.zip "$bundle_dir"
